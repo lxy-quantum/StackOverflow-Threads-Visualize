6081a1d2f5a797564efb6e8649b63f72e5265aec
lxy-quantum <3097962476@qq.com>
2024-12-10 09:21:56 +0800

    init commit

---
+++ src/main/java/org/sustech/project/StackOverflowThreadsVisualizeApplication.java
@@ -0,0 +1,13 @@
+package org.sustech.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflowThreadsVisualizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverflowThreadsVisualizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sustech/project/StackOverflowThreadsVisualizeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sustech.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverflowThreadsVisualizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
